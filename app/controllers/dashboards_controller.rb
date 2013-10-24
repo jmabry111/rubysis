121aa5091544675f536bccc11943567e088b934d
Jason Mabry <jmabry@mymabry.com>
2013-10-24 14:59:27 -0400

    tidy up dashboard sections part

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   
   def index
-    @sections = Section.where(semester_id: current_semester.id)
+    @sections = Section.where(semester_id: current_semester.id).sort! {|a,b| a.course.course_name <=> b.course.course_name}
   end
   
 end
