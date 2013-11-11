6f9d722d978441dd39e33c04599bdb6874efc6e3
Jason Mabry <jmabry@mymabry.com>
2013-11-11 14:38:59 -0500

    remove stuff from attendance_manager

--- app/models/attendance_manager.rb
+++ app/models/attendance_manager.rb
@@ -4,11 +4,7 @@ class AttendanceManager
   end
 
   def attendances
-    if @attendances.any?
-      @attendances ||= get_attendances
-    else
-      
-    end
+    @attendances ||= get_attendances
   end
 
   def get_attendances
@@ -25,21 +21,5 @@ class AttendanceManager
       Attendance.where(day: Date.today-11...Date.today+1, student_section_enrollment_id: ids).group_by(&:student_section_enrollment)
     end
   end
-  
-  def empty_attendances
-    empty_days = []
-    @section.student_section_enrollments.each do
-      if Date.today.strftime('%a') == "Mon"
-        empty_days + (Date.today-7..Date.today+1).to_a
-      elsif Date.today.strftime('%a') == "Tue"
-        empty_days + (Date.today-8..Date.today+1).to_a
-      elsif Date.today.strftime('%a') == "Wed"
-        empty_days + (Date.today-9..Date.today+1).to_a
-      elsif Date.today.strftime('%a') == "Thu"
-        empty_days + (Date.today-10..Date.today+1).to_a
-      else
-        empty_days + (Date.today-11..Date.today+1).to_a 
-      end
-    end
-  end
+
 end
